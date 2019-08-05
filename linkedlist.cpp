dd65e69b545f11cb025db6d05c01662b987c6bff
devanshi111 <48769512+devanshi111@users.noreply.github.com>
2019-08-05 16:36:56 +0530

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,141 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int x, y;
+	struct node* ptr;
+};
+
+struct node* start;
+
+struct node* newNode(int x1, int y1)
+{
+	struct node *temp=(struct node *)malloc(sizeof(struct node));
+	temp->x=x1;
+	temp->y=y1;
+	temp->ptr=NULL;
+	return temp;
+}
+
+int AddFirst(int x1, int y1)
+{
+	if (start==NULL){start=newNode(x1, y1);return 0;}
+	struct node* temp=newNode(x1, y1);
+	temp->ptr=start;
+	start=temp;
+	return 0;
+}
+
+int DelFirst()
+{
+	if(start==NULL)return 0;
+	struct node* temp=start;
+	start=temp->ptr;
+	free(temp);
+	return 0;
+}
+
+int Del(int x1, int y1)
+{
+	if(start==NULL)return 0;
+	
+	if(start->x==x1&&start->y==y1)
+	{
+		struct node* temp=start;
+		start=temp->ptr;
+		free(temp);
+	}
+	struct node* temp=start->ptr;
+	struct node* prev=start;
+	while(temp!=NULL)
+	{
+		if(temp->x==x1&&temp->y==y1)
+		{
+			prev->ptr=temp->ptr;
+			free(temp);
+			return 0;
+		}
+		prev=temp;
+		temp=temp->ptr;
+	}
+	if(temp==NULL)return -1;
+	//return 0;
+}
+
+void Search(float d)
+{
+	struct node* temp=start;
+	while(temp!=NULL)
+	{
+		int dist=(temp->x)*(temp->x)+(temp->y)*(temp->y);
+		if(dist<=d*d){cout<<"("<<temp->x<<", "<<temp->y<<") ";}
+		temp=temp->ptr;
+	}
+}
+
+bool Search_pt(int x1, int y1)
+{
+	struct node* temp=start;
+	while(temp!=NULL)
+	{
+		if(temp->x==x1&&temp->y==y1)return true;temp=temp->ptr;
+	}
+	return false;
+}
+
+int length()
+{
+	int c=0;
+	struct node* temp=start;
+	while(temp!=NULL)
+	{
+		temp=temp->ptr;
+		c++;
+	}
+	return c;
+}
+
+int main()
+{
+	start=NULL;
+	long long int q;
+	cin>>q;
+	while(q--)
+	{
+		int a, b, c;
+		cin>>a;
+		if(a==1)
+		{
+			cin>>b>>c;
+			AddFirst(b, c);
+		}
+		else if(a==2)
+		{
+			DelFirst();
+		}
+		else if(a==3)
+		{
+			cin>>b>>c;
+			Del(b, c);
+		}
+		else if(a==4)
+		{
+			float f;
+			cin>>f;
+			Search(f);
+			cout<<endl;
+		}
+		else if(a==5)	
+		{
+			cin>>b>>c;
+			if(Search_pt(b,c))cout<<"True";
+			else{cout<<"False";}
+			cout<<endl;
+		}
+		else if(a==6)
+			{cout<<length()<<endl;}
+
+	}
+	return 0;
+}
