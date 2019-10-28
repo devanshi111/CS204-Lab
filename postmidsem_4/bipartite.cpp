8d27a19ddd58434660268e07c73d0b923d490f8b
devanshi111 <48769512+devanshi111@users.noreply.github.com>
2019-10-28 16:30:41 +0530

    Create bipartite.cpp

---
+++ postmidsem_4/bipartite.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool bip(vector<vector <int>> v, int n)
+{
+	bool vis[n]={0};
+	queue <int> q;
+	int col[n]={0};
+	for (int i=0;i<n;++i)
+	{
+		//q.push(i);
+		//q.pop();
+		if(vis[i])continue;
+		else
+		{
+			col[i]=1;
+			for (int j=0;j<v[i].size();++j)
+			{
+				if(col[v[i][j]]==col[i])return false;
+				col[v[i][j]]=0-col[i];
+				q.push(v[i][j]);
+			}
+			vis[i]=1;
+		}
+		while(!q.empty())
+		{
+			int u=q.front();
+			q.pop();
+			if(vis[u])continue;
+			else
+			{
+			for (int j=0;j<v[i].size();++j)
+			{
+				if(col[v[u][j]]==col[u])return false;
+				col[v[u][j]]=0-col[u];
+				q.push(v[u][j]);
+			}
+			vis[u]=1;
+		}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int n, m;
+	cin>>n>>m;
+	vector <vector <int>> adj(n);
+	for (int i=0;i<m;++i)
+	{
+		int a, b;
+		cin>>a>>b;
+		adj[a-1].push_back(b-1);
+		adj[b-1].push_back(a-1);
+	}
+	if(bip(adj, n))cout<<"Graph is bipartite"<<endl;
+	else cout<<"Graph is not bipartite"<<endl;
+	return 0;
+}
